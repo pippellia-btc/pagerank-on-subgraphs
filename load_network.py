236185b5f9e1017d3ab4a9b8989b472eda503832
pippellia <108896743+pippellia-btc@users.noreply.github.com>
2024-07-18 12:53:01 +0200

    Create load_network.py

---
+++ load_network.py
@@ -0,0 +1,17 @@
+def load_network(name):
+
+    if type(name) != str:
+        name = str(name)
+
+    # loading the index_map
+    with open('index_map_' + name + '.json', 'r') as f:
+        index_map = json.load(f)
+    
+    # loading the JSON for the graph
+    with open('network_graph_' + name + '.json', 'r') as f:
+        data = json.load(f)
+
+    # Convert JSON back to graph
+    network_graph = nx.node_link_graph(data)
+
+    return index_map, network_graph
